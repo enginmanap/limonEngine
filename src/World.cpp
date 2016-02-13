0d24897b754171248ddeae4695379aac98a36c4a
Engin Manap <enginmanap@gmail.com>
2016-02-13 21:26:39 +0200

    add multiple objects to the world.
    This is for testing purposes.

--- src/World.cpp
+++ src/World.cpp
@@ -7,17 +7,46 @@
 
 World::World(GLHelper* glHelper){
     this->glHelper = glHelper;
+
     Model *star = new Model(glHelper);
-    star->setWorldTransform(glm::translate(glm::mat4(1.0f),glm::vec3(0.5f, 0.3f, -3.0f)));
+    star->setWorldTransform(glm::translate(glm::mat4(1.0f),glm::vec3(1.0f, 1.0f, -3.0f)));
+    objects.push_back(star);
+
+    star = new Model(glHelper);
+    star->setWorldTransform(glm::translate(glm::mat4(1.0f),glm::vec3(-1.0f, -1.0f, -3.0f)));
+    objects.push_back(star);
+
+    star = new Model(glHelper);
+    star->setWorldTransform(glm::translate(glm::mat4(1.0f),glm::vec3(1.0f, -1.0f, -3.0f)));
+    objects.push_back(star);
+
+    star = new Model(glHelper);
+    star->setWorldTransform(glm::translate(glm::mat4(1.0f),glm::vec3(-1.0f, 1.0f, -3.0f)));
     objects.push_back(star);
 }
 
 void World::play(Uint32 ticks){
     float rotation = ticks / 5000.0f * 3.14f * 2;
     glm::mat4 transform;
-    transform = glm::translate(glm::mat4(1.0f),glm::vec3(0.5f, 0.3f, -3.0f));
+    transform = glm::translate(glm::mat4(1.0f),glm::vec3(1.0f, 1.0f, -3.0f));
     transform *= glm::rotate(glm::mat4(1.0f),rotation, glm::vec3(0.0f, 1.0f, 0.0f));
     objects[0]->setWorldTransform(transform);
+
+    transform = glm::translate(glm::mat4(1.0f),glm::vec3(-1.0f, -1.0f, -3.0f));
+    transform *= glm::rotate(glm::mat4(1.0f),rotation, glm::vec3(0.0f, -1.0f, 0.0f));
+    objects[1]->setWorldTransform(transform);
+
+    float sinTic = fabs(sin(ticks/1000.0f * 3.14));
+
+    transform = glm::translate(glm::mat4(1.0f),glm::vec3(1.0f, -1.0f, -3.0f));
+    transform *= glm::scale(glm::mat4(1.0f),glm::vec3(sinTic, sinTic, sinTic));
+    objects[2]->setWorldTransform(transform);
+
+    float cosTic = fabs(cos(ticks/1000.0f * 3.14));
+
+    transform = glm::translate(glm::mat4(1.0f),glm::vec3(-1.0f, 1.0f, -3.0f));
+    transform *= glm::scale(glm::mat4(1.0f),glm::vec3(cosTic, cosTic, cosTic));
+    objects[3]->setWorldTransform(transform);
 }
 
 void World::render(){
