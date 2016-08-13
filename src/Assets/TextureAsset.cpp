1e008e227e6f41cfaaeda8ef82f1204646f47b14
Engin Manap <enginmanap@gmail.com>
2016-08-13 19:17:14 +0300

    change TextureAsset log messages to more meaningful ones.

--- src/Assets/TextureAsset.cpp
+++ src/Assets/TextureAsset.cpp
@@ -17,10 +17,11 @@ TextureAsset::TextureAsset(AssetManager* assetManager, const std::vector<std::st
     SDL_Surface *surface = IMG_Load(name.data());
 
     if (!surface) {
-        std::cerr << "TextureAsset Load failed for " << name << ". Error:" << std::endl << IMG_GetError << std::endl;
+        std::cerr << "TextureAsset Load from disk failed for " << name << ". Error:" << std::endl << IMG_GetError
+                  << std::endl;
         exit(1);
     } else {
-        std::cout << "TextureAsset " << name << " loaded succesfully." << std::endl;
+        std::cout << "TextureAsset " << name << " loaded from disk successfully." << std::endl;
     }
     if (surface->format->BytesPerPixel == 4) {
         textureBufferID = assetManager->getGlHelper()->loadTexture(surface->h, surface->w, GL_RGBA, surface->pixels);
