5bcaaf1d89a99446653c6d8c84b58b019f5d8079
Engin Manap <enginmanap@gmail.com>
2017-10-25 22:36:21 +0300

    dim the lights that are used.
    1,1,1 means if it get the light, it will be lit at max. This makes
    multilight scenes act weird.

--- src/World.cpp
+++ src/World.cpp
@@ -154,10 +154,10 @@ World::World(GLHelper *glHelper) : glHelper(glHelper), fontManager(glHelper) {
 
     Light *light;
 
-    light = new Light(Light::POINT, glm::vec3(1.0f, 6.0f, 0.0f), glm::vec3(1.0f, 1.0f, 1.0f));
+    light = new Light(Light::POINT, glm::vec3(1.0f, 6.0f, 0.0f), glm::vec3(0.5f, 0.5f, 0.5f));
     lights.push_back(light);
 
-    light = new Light(Light::DIRECTIONAL, glm::vec3(25.0f, 50.0f, 25.0f), glm::vec3(1.0f, 1.0f, 1.0f));
+    light = new Light(Light::DIRECTIONAL, glm::vec3(25.0f, 50.0f, 25.0f), glm::vec3(0.5f, 0.5f, 0.5f));
     lights.push_back(light);
 
 
