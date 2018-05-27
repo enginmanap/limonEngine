b8909a49371cf78ad7f6780aa8c908f76391099d
Engin Manap <enginmanap@gmail.com>
2018-05-28 00:29:45 +0300

    make the partial fill methods of save world private

--- src/WorldSaver.h
+++ src/WorldSaver.h
@@ -12,12 +12,11 @@ private:
     static bool fillObjects(tinyxml2::XMLDocument& mapFile, tinyxml2::XMLElement * objectsNode, const World* world );
     static bool fillLights(tinyxml2::XMLDocument &document, tinyxml2::XMLElement *lightsNode, const World *world);
     static bool addSky(tinyxml2::XMLDocument &document, tinyxml2::XMLElement *skyNode, const World *world);
+    static bool fillLoadedAnimations(tinyxml2::XMLDocument &document, tinyxml2::XMLElement *loadedAnimationsNode, const World *world);
+    static bool fillTriggers(tinyxml2::XMLDocument &document, tinyxml2::XMLElement *triggersNode, const World *world);
+
 public:
     static bool saveWorld(const std::string& mapName, const World* world);
-
-    static bool fillLoadedAnimations(tinyxml2::XMLDocument &document, tinyxml2::XMLElement *loadedAnimationsNode, const World *world);
-
-    static bool fillTriggers(tinyxml2::XMLDocument &document, tinyxml2::XMLElement *triggersNode, const World *world);
 };
 
 
