15f5de2e9e5c1d1d1dc9fb6e83c35d13b44c49f5
Engin Manap <enginmanap@gmail.com>
2018-06-22 01:36:50 +0300

    add action codes and animations for new sample map

---
+++ src/GamePlay/MayanLever.h
@@ -0,0 +1,30 @@
+//
+// Created by engin on 21.06.2018.
+//
+
+#ifndef LIMONENGINE_MAYANLEVER_H
+#define LIMONENGINE_MAYANLEVER_H
+
+#include "TriggerInterface.h"
+
+
+class MayanLever : public TriggerInterface {
+
+public:
+    MayanLever(LimonAPI *limonAPI);
+
+    std::vector<LimonAPI::ParameterRequest> getParameters() override;
+
+    bool run(std::vector<LimonAPI::ParameterRequest> parameters) override;
+
+    std::vector<LimonAPI::ParameterRequest> getResults() override;
+
+    std::string getName() override {
+        return "MayanLever";
+    }
+
+};
+
+
+
+#endif //LIMONENGINE_MAYANLEVER_H
