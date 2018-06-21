efd567d5f6b74efbdd3886880720c4c8bf27ccf5
Engin Manap <enginmanap@gmail.com>
2018-06-22 01:40:58 +0300

    fix camera code doesn't pass right vector correctly

--- src/CameraAttachment.h
+++ src/CameraAttachment.h
@@ -11,7 +11,7 @@
 class CameraAttachment {
 public:
     virtual bool isDirty() = 0;
-    virtual void getCameraVariables(glm::vec3 &position, glm::vec3 &center, glm::vec3& up, glm::vec3 right) = 0;
+    virtual void getCameraVariables(glm::vec3 &position, glm::vec3 &center, glm::vec3 &up, glm::vec3 &right) = 0;
 
     virtual ~CameraAttachment() {};
 };
