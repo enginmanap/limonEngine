4916c07158506abd75161469fb685f1e6c677d3f
Engin Manap <enginmanap@gmail.com>
2018-07-18 00:29:11 +0300

    fix loud noises play when sound loops

--- src/ALHelper.cpp
+++ src/ALHelper.cpp
@@ -33,14 +33,16 @@ ALHelper::ALHelper() {
 
 int ALHelper::soundManager() {
     while(running) {
-        SDL_AtomicLock(&playRequestLock);
-        for (size_t i = 0; i < playRequests.size(); ++i) {
-            auto sound = std::make_unique<PlayingSound>();
-            sound->asset = playRequests.at(i).second;
-            if(startPlay(playRequests.at(i).first, sound)) {
+        if(playRequests.size() > 0) { //this might miss a request because not locking, but I am ok with 10ms delay at most
+            SDL_AtomicLock(&playRequestLock);
+            for (size_t i = 0; i < playRequests.size(); ++i) {
+                auto sound = std::make_unique<PlayingSound>();
+                sound->asset = playRequests.at(i).second;
+                if (startPlay(playRequests.at(i).first, sound)) {
 
-                playingSounds.push_back(std::move(sound));
-                std::cout << "Playing new sound" << std::endl;
+                    playingSounds.push_back(std::move(sound));
+                    std::cout << "Playing new sound" << std::endl;
+                }
             }
         }
         playRequests.clear();
@@ -50,14 +52,48 @@ int ALHelper::soundManager() {
                 if((*iterator)->looped) {
                     std::unique_ptr<PlayingSound>& temp = (*iterator);
 
+                    alSourceStop(temp->source);
+
+                    const float xPos[]{0.0f, 15.0f, 15.0f, -15.0f, -15.0f};
+                    const float zPos[]{0.0f, 15.0f, -15.0f, -15.0f, 15.0f};
+                    //const float zPos[]{0.0f, 0.0f, 0.0f, 0.0f, 0.0f};
+                    static int refreshCount = 0;
+
+                    alSourcei(temp->source, AL_SOURCE_RELATIVE, AL_FALSE);
+                    alSource3f(temp->source, AL_POSITION, xPos[refreshCount], 0.0f, zPos[refreshCount]);
+                    refreshCount = (refreshCount+1) %5;
+                    std::cout << "Position is " << refreshCount << std::endl;
+
+                    ALuint buffers;
+                    ALint val;
+
+                    alGetSourcei(temp->source, AL_BUFFERS_PROCESSED, &val);
+                    std::cout << "processed buffers count is " << val << std::endl;
+
+                    alSourceUnqueueBuffers(temp->source, val, &buffers);
+
                     temp->sampleCountToPlay = temp->asset->getSampleCount();
                     temp->nextDataToBuffer = temp->asset->getSoundData();
+
+
+                    for (uint32_t i = 0; i < NUM_BUFFERS; ++i) {
+                        uint32_t currentPlaySize = std::min((uint64_t)temp->sampleCountToPlay, (uint64_t)BUFFER_ELEMENT_COUNT);
+                        temp->sampleCountToPlay = temp->sampleCountToPlay - currentPlaySize;
+
+                        alBufferData(temp->buffers[i], temp->format, temp->nextDataToBuffer, currentPlaySize * sizeof(int16_t), temp->asset->getSampleRate());
+                        temp->nextDataToBuffer = temp->nextDataToBuffer + currentPlaySize;
+                        ALenum error;
+                        if ((error = alGetError()) != AL_NO_ERROR) {
+                            std::cerr << "Loop audio buffer data failed!" << alGetString(error) << std::endl;
+                        }
+                    }
                     alSourceQueueBuffers(temp->source, NUM_BUFFERS, temp->buffers);
                     alSourcePlay(temp->source);
 
                     ++iterator;
                 } else {
                     iterator = playingSounds.erase(iterator);
+
                 }
             } else {
                 refreshBuffers(*iterator);
@@ -80,6 +116,10 @@ bool ALHelper::startPlay(bool looped, std::unique_ptr<PlayingSound> &sound) {
 
     alGenBuffers(NUM_BUFFERS, sound->buffers);
     alGenSources(1, &sound->source);
+
+    alSourcei(sound->source, AL_SOURCE_RELATIVE, AL_FALSE);
+    alSource3f(sound->source, AL_POSITION, -15.0f, 0.0f, -15.0f);
+
     if(alGetError() != AL_NO_ERROR) {
         std::cerr << "Audio buffer setup failed!" << std::endl;
         return false;
@@ -97,16 +137,17 @@ bool ALHelper::startPlay(bool looped, std::unique_ptr<PlayingSound> &sound) {
 
         alBufferData(sound->buffers[i], sound->format, sound->nextDataToBuffer, currentPlaySize * sizeof(int16_t), sound->asset->getSampleRate());
         sound->nextDataToBuffer = sound->nextDataToBuffer + currentPlaySize;
-        if(alGetError() != AL_NO_ERROR) {
-            std::cerr << "Audio buffer data failed!" << std::endl;
+        ALenum error;
+        if ((error = alGetError()) != AL_NO_ERROR) {
+            std::cerr << "Audio buffer data failed with error " << alGetString(error) << std::endl;
         }
     }
 
     alSourceQueueBuffers(sound->source, NUM_BUFFERS, sound->buffers);
     alSourcePlay(sound->source);
-    if(alGetError() != AL_NO_ERROR)
-    {
-        fprintf(stderr, "Error starting :(\n");
+    if(alGetError() != AL_NO_ERROR) {
+        std::cerr << "Error starting sound playback" << std::endl;
+
         return false;
     }
 
@@ -115,7 +156,7 @@ bool ALHelper::startPlay(bool looped, std::unique_ptr<PlayingSound> &sound) {
 
 
 bool ALHelper::refreshBuffers(std::unique_ptr<ALHelper::PlayingSound> &sound) {
-    if(sound->sampleCountToPlay > 0) {
+    //if(sound->sampleCountToPlay > 0) {
         ALuint buffer;
         ALint val;
 
@@ -125,7 +166,7 @@ bool ALHelper::refreshBuffers(std::unique_ptr<ALHelper::PlayingSound> &sound) {
 //        Check if OpenAL is done with any of the queued buffers
         alGetSourcei(sound->source, AL_BUFFERS_PROCESSED, &val);
         if (alGetError() != AL_NO_ERROR) {
-            fprintf(stderr, "Error getting buffer state:(\n");
+            std::cerr << "Error getting buffer state" << std::endl;
             return 1;
         }
         if (val <= 0) {
@@ -139,20 +180,24 @@ bool ALHelper::refreshBuffers(std::unique_ptr<ALHelper::PlayingSound> &sound) {
             //alBufferData(buffer, format, buf, ret, frequency);
             uint32_t currentPlaySize = std::min((uint64_t) sound->sampleCountToPlay, (uint64_t) BUFFER_ELEMENT_COUNT);
             sound->sampleCountToPlay = sound->sampleCountToPlay - currentPlaySize;
-
-            alBufferData(buffer, sound->format, sound->nextDataToBuffer, currentPlaySize * sizeof(int16_t), sound->asset->getSampleRate());
-            if ((error = alGetError()) != AL_NO_ERROR) {
-                fprintf(stderr, "Error buffering alGenBuffers : %s", alGetString(error));
-                return 1;
-            }
-            sound->nextDataToBuffer = sound->nextDataToBuffer + currentPlaySize;
-            alSourceQueueBuffers(sound->source, 1, &buffer);
-            if ((error = alGetError()) != AL_NO_ERROR) {
-                fprintf(stderr, "Error source buffering : %s", alGetString(error));
-                return 1;
+            if(currentPlaySize > 0) {
+                alBufferData(buffer, sound->format, sound->nextDataToBuffer, currentPlaySize * sizeof(int16_t),
+                             sound->asset->getSampleRate());
+                if ((error = alGetError()) != AL_NO_ERROR) {
+                    std::cerr << "Error buffering alGenBuffers : " << alGetString(error) << std::endl;
+                    return 1;
+                }
+                sound->nextDataToBuffer = sound->nextDataToBuffer + currentPlaySize;
+                alSourceQueueBuffers(sound->source, 1, &buffer);
+                if ((error = alGetError()) != AL_NO_ERROR) {
+                    std::cerr << "Error source buffering : %s" << alGetString(error) << std::endl;
+                    return 1;
+                }
             }
         }
-    }
+//    } else {
+//        std::cout << "no more sample to play" << std::endl;
+//    }
 
     return true;
 }
