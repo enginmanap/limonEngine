db693ba171519ed5a3edd184b0b941209d7e55bf
Engin Manap <enginmanap@gmail.com>
2018-08-28 17:22:09 +0300

    Fix warnings about text overflows

--- src/GameObjects/GUIButton.cpp
+++ src/GameObjects/GUIButton.cpp
@@ -16,7 +16,7 @@ GUIButton::GUIButton(uint32_t worldID, AssetManager *assetManager, LimonAPI *lim
         assetManager->getGlHelper(), assetManager, imageFiles[0]), worldID(worldID), name(name), limonAPI(limonAPI) {
     this->imageFiles = imageFiles;
     this->images[0] = this->image;
-    strncpy(GUINameBuffer, this->name.c_str(), sizeof(GUINameBuffer));
+    strncpy(GUINameBuffer, this->name.c_str(), sizeof(GUINameBuffer) - 1);
     strncpy(GUIFileNameBuffer[0], this->imageFiles[0].c_str(), sizeof(GUIFileNameBuffer[0]));
 
     if(this->imageFiles.size() > 1) {
