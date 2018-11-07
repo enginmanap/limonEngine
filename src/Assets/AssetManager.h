b6c2678709849d8e67c6a283d0b9df3ef554c875
Engin Manap <enginmanap@gmail.com>
2018-11-07 20:36:07 +0300

    Add support for Embedded textures

--- src/Assets/AssetManager.h
+++ src/Assets/AssetManager.h
@@ -20,11 +20,29 @@ class ALHelper;
 class AssetManager {
 public:
     enum AssetTypes { Asset_type_MODEL, Asset_type_TEXTURE, Asset_type_SKYMAP, Asset_type_SOUND };
+
+    struct EmbeddedTexture {
+        char format[9];
+        uint32_t height;
+        uint32_t width;
+        uint8_t * texelData;
+
+        bool checkFormat(const char* s) const {
+            if (nullptr == s) {
+                return false;
+            }
+            return (0 == ::strncmp(format, s, sizeof(format)));
+        }
+        ~EmbeddedTexture() {
+            delete texelData;
+        }
+    };
 private:
     const std::string ASSET_EXTENSIONS_FILE = "./engine/assetExtensions.xml";
 
     //second of the pair is how many times load requested. prework for unload
     std::map<const std::vector<std::string>, std::pair<Asset *, uint32_t>> assets;
+    std::unordered_map<std::string, std::vector<EmbeddedTexture>> embeddedTextures;
     uint32_t nextAssetIndex = 1;
 
     std::map<std::string, AssetTypes> availableAssetsList;//this map should be ordered, or editor list order would be unpredictable
@@ -75,12 +93,31 @@ public:
             Asset *assetToRemove = assets[files].first;
             delete assetToRemove;
             assets.erase(files);
+            if(embeddedTextures.find(files[0]) != embeddedTextures.end()) {
+                embeddedTextures.erase(files[0]);
+            }
         }
     }
 
     const std::map<std::string, AssetTypes>& getAvailableAssetsList() {
         return availableAssetsList;
     };
+
+    void addEmbeddedTextures(const std::string& ownerAsset, std::vector<EmbeddedTexture> textures) {
+        this->embeddedTextures[ownerAsset] = textures;
+    }
+
+    const EmbeddedTexture* getEmbeddedTextures(const std::string& ownerAsset, uint32_t textureID) {
+        if(embeddedTextures.find(ownerAsset) == embeddedTextures.end()) {
+            return nullptr;
+        }
+        if(embeddedTextures[ownerAsset].size() <= textureID) {
+            return nullptr;
+        }
+
+        return &embeddedTextures[ownerAsset][textureID];
+
+    }
 
 
     GLHelper *getGlHelper() const {
