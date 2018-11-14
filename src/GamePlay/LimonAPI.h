fe81d75e17831be7c8f03d5a21c76a217b2f6f94
Engin Manap <enginmanap@gmail.com>
2018-11-15 02:08:52 +0300

    Fix operator[] doesn't work as it should for Vec4/Mat4

--- src/GamePlay/LimonAPI.h
+++ src/GamePlay/LimonAPI.h
@@ -50,16 +50,16 @@ public:
         Vec4(float x, float y, float z): x(x), y(y), z(z), w(0) {}
 
         float operator [] (int i) const {switch (i) {
-                case 1: return x;
-                case 2: return y;
-                case 3: return z;
-                case 4: return w;
+                case 0: return x;
+                case 1: return y;
+                case 2: return z;
+                case 3: return w;
         }}
         float& operator [] (int i) {switch (i) {
-                case 1: return x;
-                case 2: return y;
-                case 3: return z;
-                case 4: return w;
+                case 0: return x;
+                case 1: return y;
+                case 2: return z;
+                case 3: return w;
             }}
     };
     struct Mat4 {
