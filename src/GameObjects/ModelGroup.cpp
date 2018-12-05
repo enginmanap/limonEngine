cc472501d65b2c0c1de85bf353565cd7fd1ee71c
Engin Manap <enginmanap@gmail.com>
2018-12-05 03:03:21 +0300

    Fix issues of wrong translate on model group
    It turns out I didn't check for scaled and rotated parents, and
    since they effect translate, ıt should have been handled.

--- src/GameObjects/ModelGroup.cpp
+++ src/GameObjects/ModelGroup.cpp
@@ -163,3 +163,38 @@ ModelGroup *ModelGroup::deserialize(GLHelper *glHelper, AssetManager *assetManag
 
     return modelGroup;
 }
+
+void ModelGroup::addToGroup(PhysicalRenderable *renderable) {
+    glm::vec3 averageTranslateDifference(0.0f, 0.0f, 0.0f);
+    if(renderables.size() != 0) {
+        for (auto iterator = renderables.begin(); iterator != renderables.end(); ++iterator) {
+            averageTranslateDifference += (*iterator)->getTransformation()->getTranslateSingle();//single because we are the parent
+        }
+    }
+
+    averageTranslateDifference += renderable->getTransformation()->getTranslateSingle() - this->transformation.getTranslate();//if already had parent, don't use it
+
+    averageTranslateDifference = averageTranslateDifference * (1.0f / (renderables.size()+1)); //+1 because new renderable is not put in yet
+    //at this point, we know where the model group imguizmo should be, now move it, and update old children
+
+    glm::vec3 difference = averageTranslateDifference;
+    for (auto iterator = renderables.begin(); iterator != renderables.end(); ++iterator) {
+        (*iterator)->getTransformation()->setTransformationsNotPropagate((*iterator)->getTransformation()->getTranslateSingle() - difference);
+    }
+
+    this->getTransformation()->addTranslate(averageTranslateDifference);
+    glm::vec3 translate, scale;
+    glm::quat orientation;
+    this->transformation.getDifference(*renderable->getTransformation(), translate, scale, orientation);
+
+    translate = translate * (1.0f / this->transformation.getScale());
+    translate = translate * this->transformation.getOrientation();
+
+    renderable->getTransformation()->setTranslate(translate);
+    renderable->getTransformation()->setScale(scale);
+    renderable->getTransformation()->setOrientation(orientation);
+    renderable->getTransformation()->setParentTransform(&this->transformation);
+    renderable->setParentObject(this);
+
+    renderables.push_back(renderable);
+}
