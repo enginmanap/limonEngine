1beb1b3911c40014639ba853508adf17435b90cc
Engin Manap <enginmanap@gmail.com>
2018-12-13 01:47:22 +0300

    Formatting on ssao blur shader

--- Engine/Shaders/SSAO/blurFragment.glsl
+++ Engine/Shaders/SSAO/blurFragment.glsl
@@ -11,10 +11,8 @@ uniform sampler2D ssaoResultSampler;
 void main() {
     vec2 texelSize = 1.0 / vec2(textureSize(ssaoResultSampler, 0));
     float result = 0.0;
-    for (int x = -2; x < 2; ++x)
-    {
-        for (int y = -2; y < 2; ++y)
-        {
+    for (int x = -2; x < 2; ++x) {
+        for (int y = -2; y < 2; ++y) {
             vec2 offset = vec2(float(x), float(y)) * texelSize;
             result += texture(ssaoResultSampler, from_vs.textureCoordinates + offset).r;
         }
