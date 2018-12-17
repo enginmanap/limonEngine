3c4175a1b899f40d247ea8249abb6a1e270c6900
Engin Manap <enginmanap@gmail.com>
2018-12-17 23:11:36 +0300

    Update AABB for physics if object is updated
    I already update physics with setTranslate/Scale/Orientation but
    sometimes object pickup doesn't work until physics is stepped. I am
    trying to figure out the cause of it.

--- src/World.cpp
+++ src/World.cpp
@@ -1201,6 +1201,7 @@ void World::ImGuiFrameSetup() {//TODO not const because it removes the object. S
             if(pickedObject->getTypeID() == GameObject::MODEL) {
                 Model* selectedObject = dynamic_cast<Model*>(pickedObject);
                 if(objectEditorResult.updated) {
+                    dynamicsWorld->updateSingleAabb(selectedObject->getRigidBody());
                     updatedModels.push_back(selectedObject);
                 }
                 if(activeAnimations.find(selectedObject) != activeAnimations.end()) {
