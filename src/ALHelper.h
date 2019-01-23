c4e10f043cc120e13f85bedd8808d54caf6a3cce
Engin Manap <enginmanap@gmail.com>
2019-01-23 04:15:42 +0300

    Add method to get proper strings of OpenAL errors

--- src/ALHelper.h
+++ src/ALHelper.h
@@ -103,6 +103,25 @@ class ALHelper {
         this->resumed = true;
     }
 
+    std::string getErrorString(ALenum error ) {
+        switch (error) {
+            case AL_NO_ERROR:
+                return "No error";
+            case AL_INVALID_NAME:
+                return "invalid name";
+            case AL_INVALID_ENUM:
+                return "invalid enum";
+            case AL_INVALID_VALUE:
+                return "invalid value";
+            case AL_INVALID_OPERATION:
+                return "invalid operaion";
+            case AL_OUT_OF_MEMORY:
+                return "out of memory";
+            default:
+                return "unknown error";
+        }
+    }
+
 public:
     ALHelper();
 
