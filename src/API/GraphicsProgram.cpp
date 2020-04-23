5823207a6ec2b72059c7fbcee181f260c0a47bf8
Engin Manap <enginmanap@gmail.com>
2020-04-23 20:08:35 +0200

    Move de/serialize methods of Graphics Program to cpp file

--- src/API/GraphicsProgram.cpp
+++ src/API/GraphicsProgram.cpp
@@ -59,3 +59,95 @@ void GraphicsProgram::setSamplersAndUBOs() {
     graphicsWrapper->attachModelUBO(getID());
     graphicsWrapper->attachModelIndicesUBO(getID());
 }
+
+
+std::shared_ptr<GraphicsProgram> GraphicsProgram::deserialize(tinyxml2::XMLElement *programNode, GraphicsInterface* graphicsWrapper) {
+    std::string vertexShader;
+    std::string geometryShader;
+    std::string fragmentShader;
+
+    tinyxml2::XMLElement* programNodeAttribute = programNode->FirstChildElement("VertexShader");
+    if (programNodeAttribute != nullptr) {
+        if(programNodeAttribute->GetText() == nullptr) {
+            std::cerr << "GLSL Program vertex shader has no text, this case is not handled!" << std::endl;
+            return nullptr;
+        } else {
+            vertexShader = programNodeAttribute->GetText();
+        }
+    }
+
+    programNodeAttribute = programNode->FirstChildElement("GeometryShader");
+    if (programNodeAttribute != nullptr) {
+        if(programNodeAttribute->GetText() == nullptr) {
+            std::cout << "GLSL Program geometry shader has no text." << std::endl;
+        } else {
+            geometryShader = programNodeAttribute->GetText();
+        }
+    }
+
+    programNodeAttribute = programNode->FirstChildElement("FragmentShader");
+    if (programNodeAttribute != nullptr) {
+        if(programNodeAttribute->GetText() == nullptr) {
+            std::cerr << "GLSL Program vertex shader has no text, this case is not handled!" << std::endl;
+            return nullptr;
+        } else {
+            fragmentShader = programNodeAttribute->GetText();
+        }
+    }
+
+    bool materialRequired = false;
+    programNodeAttribute = programNode->FirstChildElement("MaterialRequired");
+    if (programNodeAttribute != nullptr) {
+        if(programNodeAttribute->GetText() == nullptr) {
+            std::cerr << "GLSL Program material required flag couldn't be read, assuming no!" << std::endl;
+        } else {
+            std::string materialRequiredString = programNodeAttribute->GetText();
+            if(materialRequiredString == "True") {
+                materialRequired = true;
+            } else if(materialRequiredString == "False") {
+                materialRequired = false;
+            } else {
+                std::cerr << "GLSL Program material required flag is unknown, assuming no!" << std::endl;
+            }
+        }
+    } else {
+        std::cerr << "GLSL Program material required flag not found, assuming no!" << std::endl;
+    }
+    std::shared_ptr<GraphicsProgram> newProgram;
+    if(geometryShader.length() > 0 ) {
+        newProgram = graphicsWrapper->createGraphicsProgram(vertexShader, geometryShader, fragmentShader, materialRequired);
+    } else {
+        newProgram = graphicsWrapper->createGraphicsProgram(vertexShader, fragmentShader, materialRequired);
+    }
+    if(materialRequired) {
+        newProgram->setSamplersAndUBOs();
+    }
+    return newProgram;
+}
+
+bool GraphicsProgram::serialize(tinyxml2::XMLDocument &document, tinyxml2::XMLElement *parentNode) {
+    tinyxml2::XMLElement *programNode = document.NewElement("GraphicsProgram");
+    parentNode->InsertEndChild(programNode);
+    tinyxml2::XMLElement *currentElement = nullptr;
+
+    currentElement = document.NewElement("VertexShader");
+    currentElement->SetText(this->vertexShader.c_str());
+    programNode->InsertEndChild(currentElement);
+
+    currentElement = document.NewElement("GeometryShader");
+    currentElement->SetText(this->geometryShader.c_str());
+    programNode->InsertEndChild(currentElement);
+
+    currentElement = document.NewElement("FragmentShader");
+    currentElement->SetText(this->fragmentShader.c_str());
+    programNode->InsertEndChild(currentElement);
+
+    currentElement = document.NewElement("MaterialRequired");
+    if(materialRequired) {
+        currentElement->SetText("True");
+    } else {
+        currentElement->SetText("False");
+    }
+    programNode->InsertEndChild(currentElement);
+    return true;
+}
