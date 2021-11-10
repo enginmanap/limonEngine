27b660de4a9229ac625cd1ea4ef0463281090087
Engin Manap <enginmanap@gmail.com>
2021-11-10 21:59:45 +0100

    Add depth awareness to SSAO blur
    it is pretty dumb, but still clearly better than the older version

--- Engine/Shaders/SSAOBlur/fragment.glsl
+++ Engine/Shaders/SSAOBlur/fragment.glsl
@@ -9,13 +9,22 @@ in VS_FS {
 uniform sampler2D pre_ssaoResult;
 
 void main() {
+
+//we want depth aware
+	float selfDepth = texture(pre_ssaoResult, from_vs.textureCoordinates).r;
+
     vec2 texelSize = 1.0 / vec2(textureSize(pre_ssaoResult, 0));
     float result = 0.0;
     for (int x = -2; x < 2; ++x) {
         for (int y = -2; y < 2; ++y) {
             vec2 offset = vec2(float(x), float(y)) * texelSize;
-            result += texture(pre_ssaoResult, from_vs.textureCoordinates + offset).r;
+			float sampleDepth = texture(pre_ssaoResult, from_vs.textureCoordinates + offset).r;
+			if(abs(sampleDepth - selfDepth) < 0.4) {
+				result += sampleDepth;
+			} else {
+				result += selfDepth;
+			}
         }
     }
-    occlusion = result / (5.0 * 5.0);
+    occlusion = result / (5.0 *5.0 );
 }
