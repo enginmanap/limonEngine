8012339cec1cda85d1a75dd0e979f555d0eb85d1
Engin Manap <enginmanap@gmail.com>
2024-09-19 21:01:24 +0200

    Add precompile headers support
    Seems like the speed-up is minimal.
    Need to test on slower cpu.

---
+++ src/stdafx.h
@@ -0,0 +1,43 @@
+//
+// Created by engin on 18/05/2024.
+//
+
+#ifndef LIMONENGINE_STDAFX_H
+#define LIMONENGINE_STDAFX_H
+
+#include <algorithm>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <cassert>
+#include <cerrno>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <memory>
+#include <random>
+#include <string>
+#include <set>
+#include <vector>
+#include <random>
+#include <utility>
+#include <unordered_map>
+#include <unordered_set>
+#include <map>
+#include <functional>
+#include <cstdint>
+#include <cereal/access.hpp>
+#include <random>
+#include <glm/vec3.hpp>
+#include <list>
+
+#include <glm/gtc/quaternion.hpp>
+#include <glm/glm.hpp>
+#include <tinyxml2.h>
+#include "../libs/consthash/include/consthash/all.hxx"
+#include "../libs/consthash/include/consthash/cityhash64.hxx"
+#include "../libs/consthash/include/consthash/cityhash128.hxx"
+#include "../libs/consthash/include/consthash/cityhash_util.hxx"
+
+#endif //LIMONENGINE_STDAFX_H
